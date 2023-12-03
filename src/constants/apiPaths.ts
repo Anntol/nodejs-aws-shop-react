1dde3d3e1573eb6d80dcb50c23ac6eff927f06ee
Anna Tolstova <anna_tolstova@hotmail.com>
2023-12-04 01:24:56 +0200

    feat: add import handler

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://8lj211t299.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://kj1maaba55.execute-api.eu-west-1.amazonaws.com/prod",
+  import: "https://5enws3ji9e.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
