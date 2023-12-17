c96640cd0039df353c59d5f8bb295315033bece1
Anna Tolstova <anna_tolstova@hotmail.com>
2023-12-17 19:59:02 +0200

    feat: add alerts for 401 and 403 statuses

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,52 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios, { AxiosError } from "axios";
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error instanceof AxiosError && error.response) {
+      const statusCode: number = error.response.status;
+      switch (statusCode) {
+        case 401:
+          alert("Unauthorized request. Please provide Autorization header");
+          break;
+        case 403:
+          alert("Forbidden request. Please provide valid credentials");
+          break;
+        default:
+          console.error(error.message);
+      }
+    } else {
+      console.error(error);
+    }
+    return Promise.reject(error);
+  }
+);
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchOnWindowFocus: false, retry: false, staleTime: Infinity },
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: Infinity,
+      /*queryFn: async ({ queryKey }) => {
+        const response = await api.get(queryKey[0] as string);
+        return response.data;
+      },*/
+    },
   },
+  /*queryCache: new QueryCache({
+    onError: (error: unknown) => {
+      console.error("cache error: ", error);
+      if (error instanceof AxiosError) {
+        alert(`Something went wrong: ${error.message}`);
+      }
+    },
+  }),*/
 });
 
 if (import.meta.env.DEV) {
